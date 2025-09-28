6419c4783c3a963c2588dd24fd457dc70770fa0f
janani <jan200618jk@gmail.com>
2025-09-28 12:32:54 +0530

    Delete backend directory

--- backend/src/middlewares/auth.js
+++
@@ -1,9 +0,0 @@
-
-const jwt = require('jsonwebtoken');
-module.exports = function(req,res,next){
-  const auth = req.header('Authorization');
-  if(!auth) return res.status(401).json({message:'No token'});
-  const token = auth.replace('Bearer ','')
-  try{ const payload = jwt.verify(token, process.env.JWT_SECRET); req.user = payload; next(); }
-  catch(err){ res.status(401).json({message:'Invalid token'}); }
-};
--- backend/src/models/Attempt.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-const attemptSchema = new mongoose.Schema({
-  quiz: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz' },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  answers: [Number],
-  score: Number
-}, { timestamps: true });
-module.exports = mongoose.model('Attempt', attemptSchema);
